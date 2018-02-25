c315ecf7dec896f27a843e0f3ddf3162ecd820f3
miguel <mallquilim@gmail.com>
2018-02-25 16:29:38 -0500

    comit de creacion package activity

---
+++ VotaInformadoPe/app/src/main/java/com/sdkpalestra/votainformadope/activity/apoyaTuCandidato.kt
@@ -0,0 +1,13 @@
+package com.sdkpalestra.votainformadope.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.sdkpalestra.votainformadope.R
+
+class apoyaTuCandidato : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apoya_tu_candidato)
+    }
+}
